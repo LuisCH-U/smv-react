4ac9a239cea2286bf7b80190e7a2ce41ae3342b7
LuisCH-U <luis13chipana@gmail.com>
2025-02-06 22:48:45 -0500

    add video youtube

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
 
   const yesButtonClick = async () => {
     setIsModalOpenYes(true);
+
     
-    // Enviar correo electrónico
     try {
       const result = await emailjs.send('service_ushk2wj', 'template_93hvbya', {
         to_email: 'luis13chipana@gmail.com',
@@ -39,8 +39,8 @@ function App() {
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
-    const newLeft = Math.random() * (windowWidth - 100); // Resta el ancho del botón
-    const newTop = Math.random() * (windowHeight - 50); // Resta la altura del botón
+    const newLeft = Math.random() * (windowWidth - 100);
+    const newTop = Math.random() * (windowHeight - 50); 
     setButtonPosition({ left: `${newLeft}px`, top: `${newTop}px` });
   }
 
@@ -50,69 +50,83 @@ function App() {
   }
 
   return (
-    <>
-      <div>
-        <a href="https://www.youtube.com/watch?v=MiAoetOXKcY&list=RDGMEM6ijAnFTG9nX1G-kbWBUCJAVMMiAoetOXKcY&start_radio=1" target="_blank">
-          <img src={flowerLogo} className="logo flower" alt="flower logo" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=MiAoetOXKcY&list=RDGMEM6ijAnFTG9nX1G-kbWBUCJAVMMiAoetOXKcY&start_radio=1" target="_blank">
-          <img src={travelLogo} className="logo flower" alt="travel logo" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=VPRjCeoBqrI&list=RDVPRjCeoBqrI&start_radio=1" target="_blank">
-          <img src={roseLogo} className="logo rose" alt="flower de rosa" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=MiAoetOXKcY&list=RDGMEM6ijAnFTG9nX1G-kbWBUCJAVMMiAoetOXKcY&start_radio=1" target="_blank">
-          <img src={vacatioLogo} className="logo flower" alt="vacation logo" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=VPRjCeoBqrI&list=RDVPRjCeoBqrI&start_radio=1" target="_blank">
-          <img src={flowerLogo} className="logo rose" alt="flower de rosa" />
-        </a>
-      </div>
-      <h1>Quieres ser mi San Valentín</h1>
-      <h3>Quieres pasar un dia diferente en un viaje o Full day a Laraos.</h3>
-      <div className="card" style={{ textAlign: 'center' }}>
-        <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
-          <button onClick={yesButtonClick} style={{ marginRight: '10px' }}>
-            ¡Sí, me encantaría! 💖
-          </button>
-          <button 
-            onClick={noButtonClick} 
-            style={{ position: moved ? 'absolute' : 'relative', left: buttonPosition.left, top: buttonPosition.top }}
-          >
-            No 😥
-          </button>
+    <div >
+        <div>
+          <a href="https://www.youtube.com/watch?v=MiAoetOXKcY&list=RDGMEM6ijAnFTG9nX1G-kbWBUCJAVMMiAoetOXKcY&start_radio=1" target="_blank">
+            <img src={flowerLogo} className="logo flower" alt="flower logo" />
+          </a>
+          <a href="https://www.youtube.com/watch?v=MiAoetOXKcY&list=RDGMEM6ijAnFTG9nX1G-kbWBUCJAVMMiAoetOXKcY&start_radio=1" target="_blank">
+            <img src={travelLogo} className="logo flower" alt="travel logo" />
+          </a>
+          <a href="https://www.youtube.com/watch?v=VPRjCeoBqrI&list=RDVPRjCeoBqrI&start_radio=1" target="_blank">
+            <img src={roseLogo} className="logo rose" alt="flower de rosa" />
+          </a>
+          <a href="https://www.youtube.com/watch?v=MiAoetOXKcY&list=RDGMEM6ijAnFTG9nX1G-kbWBUCJAVMMiAoetOXKcY&start_radio=1" target="_blank">
+            <img src={vacatioLogo} className="logo flower" alt="vacation logo" />
+          </a>
+          <a href="https://www.youtube.com/watch?v=VPRjCeoBqrI&list=RDVPRjCeoBqrI&start_radio=1" target="_blank">
+            <img src={flowerLogo} className="logo rose" alt="flower de rosa" />
+          </a>
         </div>
-        <p>
-          "Te quiero no solo por como eres, sino por como soy yo cuando estoy contigo." - Mario Benedetti
+        <div>
+          {/* Sección para el video de YouTube */}
+          <div className="video-container">
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/1w369oiQYbI" 
+              title="YouTube video player" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+        <h1>Quieres ser mi San Valentín</h1>
+        <h3>Quieres pasar un dia diferente en un viaje o Full day a Laraos.</h3>
+        <div className="card" style={{ textAlign: 'center' }}>
+          <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+            <button onClick={yesButtonClick} style={{ marginRight: '10px' }}>
+              ¡Sí, me encantaría! 💖
+            </button>
+            <button 
+              onClick={noButtonClick} 
+              style={{ position: moved ? 'absolute' : 'relative', left: buttonPosition.left, top: buttonPosition.top }}
+            >
+              No 😥
+            </button>
+          </div>
+          <p>
+            "Te quiero no solo por como eres, sino por como soy yo cuando estoy contigo." - Mario Benedetti
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Perdoname si no puedo decirtelo en persona.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Perdoname si no puedo decirtelo en persona.
-      </p>
 
-      {isModalOpenYes && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={closeModal}>&times;</span>
-            <h2 style={{ color: "#ff69b4" }}>¡Qué emoción! 💖</h2>
-            <p style={{ fontSize: "16px" }}>No sabes cuánto significa esto para mí. ✨</p>
-            <p>Nos espera un día increíble, lleno de risas, momentos especiales y mucha alegría. 💕</p>
-            <p>¡Nos vemos pronto! 💘</p>
+        {isModalOpenYes && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={closeModal}>&times;</span>
+              <h2 style={{ color: "#ff69b4" }}>¡Qué emoción! 💖</h2>
+              <p style={{ fontSize: "16px" }}>No sabes cuánto significa esto para mí. ✨</p>
+              <p>Nos espera un día increíble, lleno de risas, momentos especiales y mucha alegría. 💕</p>
+              <p>¡Nos vemos pronto! 💘</p>
+            </div>
           </div>
-        </div>
-      )}
-      {isModalOpenNo && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={closeModal}>&times;</span>
-            <h2 style={{ color: "#ff69b4" }}>¡Está bien! 😊</h2>
-            <p style={{ fontSize: "16px" }}>Lo importante es que seas feliz y disfrutes tu día. 💖</p>
-            <p>Tal vez en otra ocasión podamos compartir un lindo momento juntos. ✨</p>
-            <p>¡Te deseo un San Valentín lleno de alegría y amor! 💕</p>
+        )}
+        {isModalOpenNo && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={closeModal}>&times;</span>
+              <h2 style={{ color: "#ff69b4" }}>¡Está bien! 😊</h2>
+              <p style={{ fontSize: "16px" }}>Lo importante es que seas feliz y disfrutes tu día. 💖</p>
+              <p>Tal vez en otra ocasión podamos compartir un lindo momento juntos. ✨</p>
+              <p>¡Te deseo un San Valentín lleno de alegría y amor! 💕</p>
+            </div>
           </div>
-        </div>
-      )}
-    </>
+        )}
+    </div>
   )
 }
 
