d127513e81a0322218b572aaafe6863ee30c652f
LuisCH-U <luis13chipana@gmail.com>
2025-02-14 19:18:50 -0500

    new modify- youtube and second flot

--- src/App.tsx
+++ src/App.tsx
@@ -130,7 +130,7 @@ function App() {
             <iframe 
               width="560" 
               height="315" 
-              src="https://www.youtube.com/embed/0b3bZqCterA?autoplay=1&mute=0"
+              src="https://www.youtube.com/embed/f8kJa6JYK9Q?autoplay=1&mute=0"
               title="YouTube video player" 
               frameBorder="0" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
